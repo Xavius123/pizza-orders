466a85919799859aadd8ba6f75868f39d6716e7e
Dan W <dantweigel@gmail.com>
2022-09-12 13:45:39 -0500

    fixed port middleware service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,24 +2,19 @@ import axios, { AxiosPromise } from 'axios';
 import { httpMethod } from '../enums/http-method.enum';
 import { Login } from '../models';
 
-const baseURL = 'https://order-pizza-api.herokuapp.com/api/auth';
+const url = '/api/auth';
 
 export const login = (request: Login): any => {
     // console.log('login', request);
-    // const endpointUrl = 'auth';
-
-    // const headers = {
-    //     'Access-Control-Allow-Origin': 'https://localhost:8080/',
-    // };
 
     const payload = {
         username: 'test',
         password: 'test',
     };
 
-    return httpRequest()
+    return httpRequest(url, httpMethod.Post, payload)
         .then((response: any) => {
-            console.log(response);
+            console.log('access_token', response.data.access_token);
             // if (response.access_token) {
             //     const { access_token } = response.access_token;
             //     console.log("AT", access_token)
@@ -32,16 +27,15 @@ export const login = (request: Login): any => {
         .catch((err: any) => err);
 };
 
-const httpRequest = (): AxiosPromise<any> =>
+const httpRequest = (
+    endpoint: string,
+    methodType: string,
+    payload: any
+): AxiosPromise<any> =>
     axios({
-        url: '/api/auth',
-        method: 'POST',
-        data: {
-            username: 'test',
-            password: 'test',
-        },
-        withCredentials: true,
-        responseType: 'json',
+        url: endpoint,
+        method: methodType,
+        data: payload,
     })
         .then((response: any) => response)
         .catch((error: any) => error);
--- src/store/store.ts
+++ src/store/store.ts
@@ -7,6 +7,10 @@ export const store = configureStore({
         auth: AuthReducer,
         orders: OrdersReducer,
     },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 });
 
 export type AppDispatch = typeof store.dispatch;
