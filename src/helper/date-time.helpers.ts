15427e953c4ac77151ae2970948c8d9f888f0ec2
Dan W <dantweigel@gmail.com>
2022-09-14 02:03:00 -0500

    added dayjs

--- src/helper/date-time.helpers.ts
+++ src/helper/date-time.helpers.ts
@@ -1,14 +1,7 @@
-export const getDateTime = (timeStamp: string): string => {
-    let newTime = '';
-    const time = new Date(timeStamp);
-    const minutes = (time.getMinutes() < 10 ? '0' : '') + time.getMinutes();
-    const month = time.getMonth() + 1;
-    const day = time.getUTCDate();
-    const seconds = time.getUTCSeconds();
-    let hours = time.getHours();
-    hours = hours + 7;
-    const ampm = hours >= 12 ? 'pm' : 'am';
-    newTime = `${month}/${day} ${hours}:${minutes}:${seconds} ${ampm}`;
+import dayjs from 'dayjs';
 
-    return newTime;
+export const getDateTime = (timeStamp: string): string => {
+    const datetime = new Date(timeStamp);
+    const formatDateTime = dayjs(datetime).format('MM D, YYYY h:mm A');
+    return formatDateTime;
 };
